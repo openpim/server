b44be0d67f40db104c41857e450881b7635969af
openpim <team@openpim.org>
2021-07-06 20:28:51 +0300

    YM (in progress)

--- src/channels/ym/YMChannelHandler.ts
+++ src/channels/ym/YMChannelHandler.ts
@@ -2,9 +2,10 @@ import { Channel, ChannelExecution } from "../../models/channels"
 import { ChannelAttribute, ChannelCategory, ChannelHandler } from "../ChannelHandler"
 import logger from "../../logger"
 import * as temp from 'temp'
-import { FileManager } from "../../media/FileManager"
 import * as xml2js from 'xml2js'
-import * as fs from 'fs'
+import { Item } from "../../models/items"
+import { sequelize } from '../../models'
+
 
 const fsAsync = require('fs').promises
 
@@ -17,76 +18,168 @@ export class YMChannelHandler extends ChannelHandler {
     public async processChannel(channel: Channel, language: string): Promise<void> {
         logger.debug('YM channel trigered: ' + channel.identifier)
         const chanExec = await this.createExecution(channel)
-
         const context: JobContext = {log: '', result: 2}
-        let fileName = temp.path({prefix: 'openpim'})
 
-        if (channel.config.ymShopAttributes) {
-            const name = this.getValueByExpression(channel.config.ymShopAttributes.find((elem:any) => elem.id === 'name'))
-            const company = this.getValueByExpression(channel.config.ymShopAttributes.find((elem:any) => elem.id === 'company'))
-            const url = this.getValueByExpression(channel.config.ymShopAttributes.find((elem:any) => elem.id === 'url'))
-            const currency1 = this.getValueByExpression(channel.config.ymShopAttributes.find((elem:any) => elem.id === 'currency1'))
+        try {
+            let fileName = temp.path({prefix: 'openpim'})
 
-            if (name && company && url && currency1) {
-                const yml:any = {yml_catalog : {$: {date: new Date().toISOString()}, shop: {name: name, company: company, url: url, currencies: []}}}
+            if (channel.config.ymShopAttributes) {
+                const name = this.getValueByExpression(channel.config.ymShopAttributes.find((elem:any) => elem.id === 'name'))
+                const company = this.getValueByExpression(channel.config.ymShopAttributes.find((elem:any) => elem.id === 'company'))
+                const url = this.getValueByExpression(channel.config.ymShopAttributes.find((elem:any) => elem.id === 'url'))
+                const currency1 = this.getValueByExpression(channel.config.ymShopAttributes.find((elem:any) => elem.id === 'currency1'))
 
-                channel.config.ymShopAttributes.forEach((attrConfig:any) => {
-                    if (attrConfig.id !== 'name' && attrConfig.id !== 'company' && attrConfig.id !== 'url') {
-                        const attr = this.getValueByExpression(channel.config.ymShopAttributes.find((elem:any) => elem.id === attrConfig.id))
-                        if (attr != null) {
-                            if (attrConfig.id.startsWith('currency')) {
-                                const arr = attr.split(',')
-                                yml.yml_catalog.shop.currencies.push({currency: {$: {id: arr[0], rate: arr[1]}}})
-                            } else if (attrConfig.id.startsWith('delivery-option')) {
-                                const arr = attr.split(',')
-                                if (!yml.yml_catalog.shop['delivery-options']) yml.yml_catalog.shop['delivery-options'] = []
-                                const option:any = {option: {$: {cost: arr[0], days: arr[1]}}}
-                                if (arr.length > 2) option.option.$['order-before'] = arr[2]
-                                yml.yml_catalog.shop['delivery-options'].push(option)
-                            } else { 
-                                yml.yml_catalog.shop[attrConfig.id] = attr
+                if (name && company && url && currency1) {
+                    const yml:any = {yml_catalog : {$: {date: new Date().toISOString()}, shop: {name: name, company: company, url: url, currencies:{ currency:[]}}}}
+
+                    channel.config.ymShopAttributes.forEach((attrConfig:any) => {
+                        if (attrConfig.id !== 'name' && attrConfig.id !== 'company' && attrConfig.id !== 'url') {
+                            const attr = this.getValueByExpression(channel.config.ymShopAttributes.find((elem:any) => elem.id === attrConfig.id))
+                            if (attr != null) {
+                                if (attrConfig.id.startsWith('currency')) {
+                                    const arr = attr.split(',')
+                                    yml.yml_catalog.shop.currencies.currency.push({$: {id: arr[0], rate: arr[1]}})
+                                } else if (attrConfig.id.startsWith('delivery-option')) {
+                                    const arr = attr.split(',')
+                                    if (!yml.yml_catalog.shop['delivery-options']) yml.yml_catalog.shop['delivery-options'] = {option: []}
+                                    const option:any = {$: {cost: arr[0], days: arr[1]}}
+                                    if (arr.length > 2) option.$['order-before'] = arr[2]
+                                    yml.yml_catalog.shop['delivery-options'].option.push(option)
+                                } else { 
+                                    yml.yml_catalog.shop[attrConfig.id] = attr
+                                }
                             }
-                        }
-                    }                    
-                })
+                        }                    
+                    })
 
-                const builder = new xml2js.Builder()
-                const str = builder.buildObject(yml)
-                logger.debug('YML file created. \n ' + str)
-                console.log(str)
-                
-                /*
-                await fsAsync.writeFile(fileName, str)
-                if (fs.existsSync(fileName)) {
-                    const fm = FileManager.getInstance()
-                    fileName = await fm.saveChannelFile(channel.tenantId, channel.id, chanExec, fileName)
-                    context.log += '\nсоздан YML файл'
-                }
+                    await this.processCategories(channel, yml, language, context)
 
-                if (channel.config.extCmd) {
-                    const cmd = channel.config.extCmd + ' ' + fileName
-                    logger.debug('Starting [' + cmd + ']')
-                    context.log += '\nЗапускается ' + cmd
-                    const result: any = await this.asyncExec(cmd)
-                    context.log += result.stdout + (result.stderr ? "\nERRORS:\n" + result.stderr : "") 
-                    if (result.code !== 0) {
-                        context.result = 3
-                        context.log += '\nОшибка запуска: ' + result.code
+                    const builder = new xml2js.Builder()
+                    const str = builder.buildObject(yml)
+                    logger.debug('YML file created. \n ' + str)
+                    console.log(str)
+                    
+                    /*
+                    await fsAsync.writeFile(fileName, str)
+                    if (fs.existsSync(fileName)) {
+                        const fm = FileManager.getInstance()
+                        fileName = await fm.saveChannelFile(channel.tenantId, channel.id, chanExec, fileName)
+                        context.log += '\nсоздан YML файл'
                     }
-                }*/
+
+                    if (channel.config.extCmd) {
+                        const cmd = channel.config.extCmd + ' ' + fileName
+                        logger.debug('Starting [' + cmd + ']')
+                        context.log += '\nЗапускается ' + cmd
+                        const result: any = await this.asyncExec(cmd)
+                        context.log += result.stdout + (result.stderr ? "\nERRORS:\n" + result.stderr : "") 
+                        if (result.code !== 0) {
+                            context.result = 3
+                            context.log += '\nОшибка запуска: ' + result.code
+                        }
+                    }*/
+                } else {
+                    context.result = 3
+                    if (!name) context.log += 'Не задан name в заголовоке YML файла'
+                    if (!company) context.log += 'Не задан company в заголовоке YML файла'
+                    if (!url) context.log += 'Не задан url в заголовоке YML файла'
+                    if (!currency1) context.log += 'Не задана валюта в заголовоке YML файла'
+                }
             } else {
                 context.result = 3
-                if (!name) context.log += 'Не задан name в заголовоке YML файла'
-                if (!company) context.log += 'Не задан company в заголовоке YML файла'
-                if (!url) context.log += 'Не задан url в заголовоке YML файла'
-                if (!currency1) context.log += 'Не задана валюта в заголовоке YML файла'
+                context.log += 'Не задан заголовок YML файла'
             }
-        } else {
+        } catch (err) {
             context.result = 3
-            context.log += 'Не задан заголовок YML файла'
+            context.log += "Ошибка: " + err.message
+        } finally {
+            await this.finishExecution(channel, chanExec, context.result, context.log)
         }
-        await this.finishExecution(channel, chanExec, context.result, context.log)
+    }
 
+    private async processCategories(channel: Channel, yml: any, language: string, context: JobContext) {
+        if (!channel.config.ymCategoryFrom) {
+            context.result = 3
+            context.log += "Не задан объект с которого начать генерацию категорий"
+            return
+        }
+        if (!channel.config.ymCategoryTypes || channel.config.ymCategoryTypes.length === 0) {
+            context.result = 3
+            context.log += "Не заданы типы для генерации категорий"
+            return
+        }
+        if (!channel.config.ymCategoryAttributes || channel.config.ymCategoryAttributes.length === 0) {
+            context.result = 3
+            context.log += "Не заданы id и Названия для категорий"
+            return
+        }
+
+        const idConfig = channel.config.ymCategoryAttributes.find((elem:any) => elem.id === 'id')
+        if (!idConfig) {
+            context.result = 3
+            context.log += "Не задано id для категорий"
+            return
+        }
+        const nameConfig = channel.config.ymCategoryAttributes.find((elem:any) => elem.id === 'name')
+        if (!nameConfig) {
+            context.result = 3
+            context.log += "Не задано название для категорий"
+            return
+        }
+        
+        const categories:any[] = []
+        yml.yml_catalog.shop.categories = { category: categories }
+
+        const itemFrom = await Item.findOne({ where: { tenantId: channel.tenantId, id: channel.config.ymCategoryFrom }})
+        if (!itemFrom) {
+            context.result = 3
+            context.log += "Не найдет объект с которого начинать по id: " + channel.config.ymCategoryFrom
+            return
+        }
+
+        const items = await sequelize.query('SELECT * FROM items where "deletedAt" IS NULL and "tenantId"=:tenant and path~:lquery order by path', {
+            replacements: { 
+                tenant: channel.tenantId,
+                lquery: itemFrom.path + '.*'
+            },
+            model: Item,
+            mapToModel: true
+        })
+
+        let parent = itemFrom
+        let previousItem = itemFrom
+        for (let i = 0; i < items.length; i++) {
+            const item = items[i];
+            if (item.id === itemFrom.id) continue
+
+            if (parent.identifier !== item.parentIdentifier) {
+                parent = previousItem
+            }
+
+            if (!channel.config.ymCategoryTypes.includes(item.typeId)) continue
+
+            const id = await this.getValueByMapping(channel, idConfig, item, language)
+            if (!id) {
+                context.result = 3
+                context.log += "Не найдет id для категории с идентификатором: " + item.identifier
+                return
+            }
+
+            const name = await this.getValueByMapping(channel, nameConfig, item, language)
+            if (!name) {
+                context.result = 3
+                context.log += "Не найдено название для категории с идентификатором: " + item.identifier
+                return
+            }
+
+            const category: any ={$: {id: id}, _: name }
+            if (parent.identifier !== itemFrom.identifier) {
+                category.$.parentId = parent.id
+            }
+            categories.push(category)
+
+            previousItem = item
+        }
     }
 
     public async getCategories(channel: Channel): Promise<ChannelCategory[]> {
