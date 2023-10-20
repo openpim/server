166503c8fe2467517b004ece84f11cd7292b8a38
Vyacheslav Pedak <vpedak@gmail.com>
2023-10-20 13:42:39 +0300

    add - to attr identifier

--- src/resolvers/attributes.ts
+++ src/resolvers/attributes.ts
@@ -146,7 +146,7 @@ export default {
             if (!context.canEditConfig(ConfigAccess.ATTRIBUTES)) 
                 throw new Error('User '+ context.getCurrentUser()?.id+ ' does not has permissions to create attribute, tenant: ' + context.getCurrentUser()!.tenantId)
 
-            if (!/^[A-Za-z0-9_]*$/.test(identifier)) throw new Error('Identifier must not has spaces and must be in English only: ' + identifier + ', tenant: ' + context.getCurrentUser()!.tenantId)
+            if (!/^[A-Za-z0-9_-]*$/.test(identifier)) throw new Error('Identifier must not has spaces and must be in English only: ' + identifier + ', tenant: ' + context.getCurrentUser()!.tenantId)
 
             const nGroupId = parseInt(groupId)
             const mng = ModelsManager.getInstance().getModelManager(context.getCurrentUser()!.tenantId)
