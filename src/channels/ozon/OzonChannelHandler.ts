9a80ad98d76d2933093357eb4d51bf7d854b042d
Vyacheslav Pedak <vpedak@gmail.com>
2024-06-11 13:44:30 +0300

    fix

--- src/channels/ozon/OzonChannelHandler.ts
+++ src/channels/ozon/OzonChannelHandler.ts
@@ -989,7 +989,7 @@ export class OzonChannelHandler extends ChannelHandler {
                     next = json.has_next
                     if (dict.length === 0) throw new Error('No data for attribute dictionary: '+ozonAttrId+', for category: '+ozonCategoryId)
                     last = dict[dict.length-1]?.id
-                    if (idx++ > 35) {
+                    if (idx++ > 50) {
                         throw new Error('Data dictionary for attribute: '+ozonAttrId+ ', typeId:' + ozonTypeId + ' is too big, for category: '+ozonCategoryId)
                     }
                 } while (next)
