ee1a4d4669be1440568de0fdd97ddc1a4a33bce9
Vyacheslav Pedak <vpedak@gmail.com>
2024-06-13 17:55:47 +0300

    extract file operation to one interface

--- src/storage/StorageFactory.ts
+++ src/storage/StorageFactory.ts
@@ -2,7 +2,12 @@ import { FilesystemStorageManager } from "./FilesystemStorageManager";
 import { StorageManager } from "./StorageManager";
 
 export class StorageFactory {
+    private static instance: StorageManager
     public static getStorageInstance(): StorageManager {
-        return new FilesystemStorageManager()
+        if (!StorageFactory.instance) {
+            StorageFactory.instance = new FilesystemStorageManager()
+        }
+
+        return StorageFactory.instance
     }    
 }
