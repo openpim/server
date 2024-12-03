d830d55d9b364e7de61c5066a08119579632a56a
Vyacheslav Pedak <vpedak@gmail.com>
2024-12-03 15:54:48 +0300

    ext channel fix

--- src/channels/ext/ExtChannelHandler.ts
+++ src/channels/ext/ExtChannelHandler.ts
@@ -9,20 +9,21 @@ import Context from "../../context"
 export class ExtChannelHandler extends ChannelHandler {
     public async processChannel(channel: Channel, language: string, data: any, context?: Context): Promise<void> {
         if (channel.config.extCmd) {
-            const chanExec = await this.createExecution(channel)
+            let chanExec: ChannelExecution | null = null
+            if (!channel.config.externalExecMng) chanExec = await this.createExecution(channel)
             const tempName = temp.path({ prefix: 'openpim' })
-            const cmd = channel.config.extCmd.replaceAll('{executionId}', chanExec.id).replaceAll('{channelIdentifier}', channel.identifier).replaceAll('{outputFile}', tempName).replaceAll('{language}', language).replaceAll('{user}', JSON.stringify(context?.getUserLogin()) || '').replaceAll('{roles}', JSON.stringify(context?.getUserRoles().join(',')) || '')
+            const cmd = channel.config.extCmd.replaceAll('{executionId}', chanExec?.id).replaceAll('{channelIdentifier}', channel.identifier).replaceAll('{outputFile}', tempName).replaceAll('{language}', language).replaceAll('{user}', JSON.stringify(context?.getUserLogin()) || '').replaceAll('{roles}', JSON.stringify(context?.getUserRoles().join(',')) || '')
             logger.info('Starting program :' + cmd + ' channel: ' + channel.identifier + ', tenant: ' + channel.tenantId)
             const result: any = await this.asyncExec(cmd)
             logger.debug('exec finished for channel: ' + channel.identifier + ', tenant: ' + channel.tenantId + ', result: ' + JSON.stringify(result))
             if (!channel.config.externalExecMng) {
                 if (fs.existsSync(tempName)) {
                     const fm = FileManager.getInstance()
-                    await fm.saveChannelFile(channel.tenantId, channel.id, chanExec, tempName)
+                    await fm.saveChannelFile(channel.tenantId, channel.id, chanExec!, tempName)
                 }
         
                 const log = result.stdout + (result.stderr ? "\nERRORS:\n" + result.stderr : "") 
-                await this.finishExecution(channel, chanExec, result.code === 0 ? 2 : 3, log)
+                await this.finishExecution(channel, chanExec!, result.code === 0 ? 2 : 3, log)
             }
         } else {
             throw new Error('Command is not defined for channel: ' + channel.identifier + ', tenant: ' + channel.tenantId)
