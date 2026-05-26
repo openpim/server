1ae00efa83bda6c7d36ef9cfaf3493e53729c287
Sket4I <itseplyaev@gmail.com>
2026-05-26 17:17:20 +0300

    fix save itemRelation on processCreateUpload

--- src/media/index.ts
+++ src/media/index.ts
@@ -471,13 +471,15 @@ export async function processCreateUpload(context: Context, req: Request, res: R
                 const irValues = {}
                 await processItemRelationActions(context, EventType.BeforeCreate, itemRelation, null, irValues, false, false, transaction)
                 await updateItemRelationAttributes(context, mng, itemRelation, false, transaction)
-                await itemRelation.save({ transaction })
+
                 if (irValues) {
                     filterValues(context.getEditItemRelationAttributes(itemRelation.relationId), irValues)
                     checkValues(mng, irValues)
                     itemRelation.values = irValues
                 }
+                await itemRelation.save({ transaction })
                 await transaction.commit()
+
                 if (audit.auditEnabled()) {
                     const itemChanges: AuditItem = {
                         added: {
